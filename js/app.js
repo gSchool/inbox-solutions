4e10cae942ce39a3d96d0f42e0aa2ce848d34fab
Jeff Dean <jeff@galvanize.it>
2015-05-12 23:10:27 -0600

    message selection is persisted across page loads

--- js/app.js
+++ js/app.js
@@ -1,23 +1,27 @@
 $(function () {
 
   // ---- INITIAL PAGE LOAD
+  // ---- Loads selection state from sessionStorage
   var templateString = $('[data-template=message]').text();
   var $messagesContainer = $("[data-container=messages]");
   $.getJSON('/api/inbox.json').then(function (data) {
     data.messages.forEach(function (message) {
+      var checked = sessionStorage.getItem('message-' + message.id);
       $messagesContainer.append(
         templateString
           .replace('{readClass}', message.read ? 'read' : 'unread')
-          .replace('{checked}', '')
+          .replace('{checked}', checked ? 'checked' : '')
           .replace('{starClass}', message.starred ? 'fa-star' : 'fa-star-o')
-          .replace('{selectedClass}', '')
+          .replace('{selectedClass}', checked ? 'selected' : '')
           .replace('{subject}', message.subject)
+          .replace('{messageId}', message.id)
       )
     });
   });
 
   // ---- User can select all messages
   // ---- User can deselect all messages
+  // ---- Changing selection is saved in sessionStorage
   $('[data-behavior=multiselect]').on('click', function () {
     var $checkboxes = $messagesContainer.find(':checkbox');
 
@@ -29,10 +33,14 @@ $(function () {
 
       $messagesContainer.find(':checkbox')
         .prop('checked', false)
-        .closest('[data-message]')
+        .closest('[data-message-id]')
         .removeClass('selected');
 
       $('[data-disableable]').prop('disabled', true);
+
+      $messagesContainer.find('[data-message-id]').each(function () {
+        sessionStorage.removeItem('message-' + $(this).data('message-id'));
+      });
     } else {
       $(this).find('i')
         .removeClass('fa-square-o')
@@ -41,19 +49,32 @@ $(function () {
 
       $messagesContainer.find(':checkbox')
         .prop('checked', true)
-        .closest('[data-message]')
+        .closest('[data-message-id]')
         .addClass('selected');
 
       $('[data-disableable]').prop('disabled', false);
+
+      $messagesContainer.find('[data-message-id]').each(function () {
+        sessionStorage.setItem('message-' + $(this).data('message-id'), true);
+      });
     }
   });
 
   // ---- User can select an individual message
   // ---- User can deselect an individual message
+  // ---- Changing selection is saved in sessionStorage
   $('[data-container=messages]').on('change', ':checkbox', function () {
-    $(this).closest('[data-message]').toggleClass('selected');
+    var $message = $(this).closest('[data-message-id]');
     var $checkboxes = $messagesContainer.find(':checkbox');
     var $multiselect = $('[data-behavior=multiselect]');
+
+    if ($(this).is(':checked')) {
+      $message.addClass('selected');
+      sessionStorage.setItem('message-' + $message.data('message-id'), true);
+    } else {
+      $message.removeClass('selected');
+      sessionStorage.removeItem( 'message-' + $message.data('message-id') );
+    }
 
     if ($checkboxes.filter(':checked').length === $checkboxes.length) {
       $multiselect.find('i')
